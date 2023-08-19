8d82056d84a95c2ab9b4cdaad674e69c0a7e1993
Đinh Văn Luận <dinhvanluan2k3@gmail.com>
2023-08-19 19:41:42 +0700

    rename routes feedback

--- backend/src/routes/v1/v1.routes.ts
+++ backend/src/routes/v1/v1.routes.ts
@@ -8,6 +8,7 @@ import productRoutes from "./products/products.routes";
 import userRoutes from "./users/user.routes";
 import uploadRoutes from "./uploadFileToCloud/uploadFileToCloud.routes";
 import cartRoutes from "./carts/cart.routes";
+import feedbackRoutes from "./feedbacks/feedback.routes";
 
 routerV1.use("/v1", adminRoutes);
 routerV1.use("/v1", categoryRoutes);
@@ -16,5 +17,6 @@ routerV1.use("/v1", productRoutes);
 routerV1.use("/v1", userRoutes);
 routerV1.use("/v1", uploadRoutes);
 routerV1.use("/v1", cartRoutes);
+routerV1.use("/v1", feedbackRoutes);
 
 export default routerV1;
