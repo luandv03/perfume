633b486c2fba892bfd5f67cbb452f8c52f08d771
Đinh Văn Luận <dinhvanluan2k3@gmail.com>
2023-09-30 19:19:11 +0700

    fix contact ui

--- frontend/client/src/components/Contact/Contact.tsx
+++ frontend/client/src/components/Contact/Contact.tsx
@@ -1,7 +1,60 @@
-import { Flex, Stack, Text, TextInput, Textarea, Button } from "@mantine/core";
-import { IconMapPin, IconPhone, IconMail } from "@tabler/icons-react";
+import {
+    Flex,
+    Stack,
+    Text,
+    TextInput,
+    Textarea,
+    Button,
+    Alert,
+} from "@mantine/core";
+import {
+    IconMapPin,
+    IconPhone,
+    IconMail,
+    IconLoader,
+} from "@tabler/icons-react";
+import { hasLength, useForm } from "@mantine/form";
+import { useState } from "react";
 
 export const Contact = () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [notification, setNotification] = useState<string>("");
+
+    const form = useForm({
+        initialValues: {
+            name: "",
+            email: "",
+            content: "",
+        },
+
+        validateInputOnBlur: true,
+
+        validate: {
+            name: hasLength({ min: 1 }, "Tên không được để trống"),
+            content: hasLength({ min: 1 }, "Nội dung không được để trống"),
+            email: (value) =>
+                /^\S+@\S+$/.test(value) ? null : "Email không hợp lệ",
+        },
+    });
+
+    const handleSendMail = async (payload: {
+        name: string;
+        email: string;
+        content: string;
+    }) => {
+        if (form.errors.name || form.errors.email || form.errors.content)
+            return;
+
+        /// call api
+        setLoading(true);
+        console.log(payload);
+        setLoading(false);
+        setNotification(
+            "Cảm ơn bạn đã liên hệ với chúng tôi. Chúng tôi sẽ liên hệ với bạn sớm nhất có thể"
+        );
+        form.reset();
+    };
+
     return (
         <Stack>
             <Flex
@@ -46,25 +99,47 @@ export const Contact = () => {
                         Bạn hãy điền đầy đủ các thông tin bên dưới và gửi cho
                         chúng tôi. Chúng tôi sẽ trả lời sau khi nhận được.
                     </Text>
+                    {!!notification.length && (
+                        <Alert color="red">
+                            <Text color="red">{notification}</Text>
+                        </Alert>
+                    )}
                     <Flex justify="space-between" gap={20}>
                         <TextInput
                             mt="md"
                             label="Họ và tên"
                             style={{ width: "50%" }}
+                            {...form.getInputProps("name")}
                         />
                         <TextInput
                             mt="md"
                             label="Email"
                             style={{ width: "50%" }}
+                            {...form.getInputProps("email")}
                         />
                     </Flex>
                     <Textarea
                         label="Nội dung"
                         style={{ width: "100%" }}
                         minRows={4}
+                        {...form.getInputProps("content")}
                     />
-                    <Button color="gray" size="sm">
-                        Gửi tin nhắn
+                    <Button
+                        maw={180}
+                        color="gray"
+                        size="sm"
+                        onClick={() => handleSendMail(form.values)}
+                        disabled={
+                            !form.values.name ||
+                            !form.values.email ||
+                            !form.values.content
+                        }
+                    >
+                        {loading ? (
+                            <IconLoader className={loading ? "spinner" : ""} />
+                        ) : (
+                            "Gửi tin nhắn"
+                        )}
                     </Button>
                 </Stack>
             </Flex>
